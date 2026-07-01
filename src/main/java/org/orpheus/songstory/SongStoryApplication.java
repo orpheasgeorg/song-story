511158cfce122c0a6ec65e35ce0c0da721988833
orpheasgeorg <orpheas21@gmail.com>
2026-07-01 13:28:31 +0300

    Initial commit

---
+++ src/main/java/org/orpheus/songstory/SongStoryApplication.java
@@ -0,0 +1,13 @@
+package org.orpheus.songstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongStoryApplication.class, args);
+	}
+
+}
