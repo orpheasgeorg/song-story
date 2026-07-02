53f80028cae738f9689b6778a474ae4d65ff509c
orpheasgeorg <orpheas21@gmail.com>
2026-07-02 21:45:42 +0300

    feat: add Genius API integration for song search

--- src/main/java/org/orpheus/songstory/SongStoryApplication.java
+++ src/main/java/org/orpheus/songstory/SongStoryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SongStoryApplication {
+public class  SongStoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SongStoryApplication.class, args);
