f877ffa42b79ed2a6d22ad3235a7aff7aff8a846
orpheasgeorg <orpheas21@gmail.com>
2026-07-06 19:01:50 +0300

    Added async Services to wrap classic Services

--- src/main/java/org/orpheus/songstory/service/async/AsyncAiService.java
+++ src/main/java/org/orpheus/songstory/service/async/AsyncAiService.java
@@ -1,6 +1,7 @@
 package org.orpheus.songstory.service.async;
 
 import org.orpheus.songstory.DTO.GeniusSearchResult;
+import org.orpheus.songstory.service.classic.AiService;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,12 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class AsyncAiService {
 
+    private AiService aiService;
+
     @Async
     public CompletableFuture<String> asyncStoryBuild(GeniusSearchResult request) {
+
+        String results = aiService.storyBuild(request);
+        return CompletableFuture.completedFuture(results);
     }
 }
--- src/main/java/org/orpheus/songstory/service/async/AsyncGeniusService.java
+++ src/main/java/org/orpheus/songstory/service/async/AsyncGeniusService.java
@@ -2,6 +2,7 @@ package org.orpheus.songstory.service.async;
 
 import org.orpheus.songstory.DTO.GeniusSearchResult;
 import org.orpheus.songstory.DTO.SongRequest;
+import org.orpheus.songstory.service.classic.GeniusService;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,13 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class AsyncGeniusService {
 
+    private GeniusService geniusService;
+
     @Async
     public CompletableFuture<List<GeniusSearchResult>> asyncSearch(SongRequest request) {
+
+        List<GeniusSearchResult> results = geniusService.search(request);
+        return CompletableFuture.completedFuture(results);
+
     }
 }
